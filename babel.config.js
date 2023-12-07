7d9302e5794381e59432b27e4e2aef7cf132ccca
LouisKo <louiskoxiang@gmail.com>
2023-12-07 11:42:51 +0800

    babel setting2

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,21 @@
 module.exports = {
   presets: [
-    '> 0.5%, last 2 versions, Firefox ESR, not dead'
+    [
+      '@vue/cli-plugin-babel/preset',
+      {
+        useBuiltIns: 'entry',
+        corejs: 3
+      }
+    ],
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          browsers: ['iOS >= 11']
+        },
+        useBuiltIns: 'entry',
+        corejs: 3
+      }
+    ]
   ]
 }
