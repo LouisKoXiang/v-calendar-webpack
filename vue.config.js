5c15d7fc70b7fe9880d339460f02ed437f6a2c80
LouisKo <louiskoxiang@gmail.com>
2023-12-07 13:59:24 +0800

    vue config setting

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  chainWebpack: config => {
+    config.module
+      .rule('js')
+      .include.add('/node_modules/v-calendar/')
+      .end()
+      .use('babel-loader')
+      .loader('babel-loader')
+  },
   transpileDependencies: true
 })
